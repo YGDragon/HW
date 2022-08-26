8690a39224903bc2d18f0e9acc93c4abef45e688
ygdragon <djsdv@tut.by>
2022-08-26 06:59:32 +0300

    Создание структуры домашнего задания

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая
+принимает на вход число (N) и
+выдаёт таблицу кубов чисел от 1 до N.
+*/
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string message = "1";
+for (int i = 2; i <= num; i++)
+{
+    message = message + ',' + " " + i * i * i;
+}
+Console.WriteLine(message);
