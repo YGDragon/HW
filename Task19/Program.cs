578e5e7f54912c2660bcb1c9e5da67c820198144
Дмитрий <109813121+YGDragon@users.noreply.github.com>
2022-08-28 04:26:02 +0300

    Update Program.cs
    Code optimization

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,23 +1,15 @@
-﻿/*
+/*
 Напишите программу, которая принимает на вход пятизначное число
 и проверяет, является ли оно палиндромом.
 14212 -> нет
 12821 -> да
 23432 -> да
 */
-Console.Write("Введите пятизначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-// Проверка на правильность ввода числа
-while (num <= 10000 || num >= 100000)
-{
-    Console.Write("Введено неверное значение. ");
-    Console.Write("Введите пятизначное число: ");
-    num = Convert.ToInt32(Console.ReadLine());
-}
 // Проверка последовательно трех чисел на соответствие палиндрому
-for (int i = 1; i < 4; i++)
+int [] num = {14212,12821,23432};
+for (int i = 0; i < 3; i++)
 {
-    int part1 = Math.DivRem(num, 10, out int d5); // Нахождение значений всех цифр в числе
+    int part1 = Math.DivRem(num[i], 10, out int d5); // Нахождение значений всех цифр в числе
     int part2 = Math.DivRem(part1, 10, out int d4);
     int part3 = Math.DivRem(part2, 10, out int d3);
     int part4 = Math.DivRem(part3, 10, out int d2);
@@ -25,17 +17,10 @@ for (int i = 1; i < 4; i++)
     int cond = d1 + 1;
     if (cond == d2 && d2 < d3 && d4 == d2 && d5 == d1) // Условие проверки на соответствие палиндрому
     {
-        Console.WriteLine($"{num} -> да");
+        Console.WriteLine($"{num[i]} -> да");
     }
     else
     {
-        Console.WriteLine($"{num} -> нет");
+        Console.WriteLine($"{num[i]} -> нет");
     }
-    // Условие прерывания ввода следующего числа
-    if (i == 3)
-    {
-        break;
-    }
-    Console.Write("Введите слудующее пятизначное число: ");
-    num = Convert.ToInt32(Console.ReadLine());
 }
