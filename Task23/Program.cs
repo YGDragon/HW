b9c8331699a24d533561894d81f1ae4e9babbb31
Дмитрий <109813121+YGDragon@users.noreply.github.com>
2022-08-28 14:26:27 +0300

    Update Program.cs
    Code optimization

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,26 +1,18 @@
-﻿/*
+/*
 Напишите программу, которая принимает на вход число (N)
 и выдаёт таблицу кубов чисел от 1 до N.
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-string cell = "1";
+int[] num = {3, 5};
 // Вычисление результата последовательно для двух чисел
-for (int n = 1; n < 3; n++)
+foreach (int k in num)
 {
-    // Формирование таблицы кубов чисел
-    for (int i = 2; i < num + 1; i++)
-    {
-        cell = cell + ',' + " " + Math.Pow(i, 3);
-    }
-    Console.WriteLine($"{num} " + "->" + " " + cell); // Вывод результата
-    // Условие прерывания ввода следующего числа
-    if (n == 2)
-    {
-        break;
-    }
-    Console.Write("Введите слудующее число: ");
-    num = Convert.ToInt32(Console.ReadLine());
+  // Формирование таблицы кубов чисел
+  string cell = "1";
+  for (int i = 2; i < k + 1; i++)
+  {
+    cell = cell + ',' + " " + Math.Pow(i, 3);
+  }
+  Console.WriteLine($"{k} ->" + " " + cell); 
 }
