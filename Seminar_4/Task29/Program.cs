e476c9b2367b0d30148f7fcc2c7a117acceb696f
Дмитрий <109813121+YGDragon@users.noreply.github.com>
2022-09-01 07:17:36 +0300

    Update Program.cs
    Code optimization

--- Seminar_4/Task29/Program.cs
+++ Seminar_4/Task29/Program.cs
@@ -5,18 +5,19 @@
 6, 1, 33 -> [6, 1, 33]
 */
 // Определение функции задания и вывода массива
-void NumberArray()
+void NumberArray(int[] num)
 {
-    int[] num = new int[8];
     string line = String.Empty;
-    for (int i = 0; i < 7; i++)
+    int l = num.Length - 1;
+    for (int i = 0; i < l; i++)
     {
         num[i] = new Random().Next(1, 40);
         line = line + $"{num[i]}, ";
     }
-    num[7] = new Random().Next(1, 40);
-    line = line + $"{num[7]}";
+    num[l] = new Random().Next(1, 40);
+    line = line + $"{num[l]}";
     Console.Write(line + " -> [" + line + "]");
 }
+int[] inputnum = new int[8];
 // Вызов функции
-NumberArray();
+NumberArray(inputnum);
