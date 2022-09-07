399120a17ea899a7cd96fb3eec1d456b1f02d01c
ygdragon <djsdv@tut.by>
2022-09-08 01:32:20 +0300

    Программы для Seminar_6 для задач №41,43

---
+++ Seminar_6/Task41/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Задача 41:
+Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+// Комбинированный метод - вывод результата.
+void OutResult()
+{
+    Console.Write($"Задать количесто чисел для ввода: ");
+    int inputAll = Convert.ToInt32(Console.ReadLine());
+    int[] numbers = new int[inputAll];
+    FillArray(numbers);
+    PrintArray(numbers);
+    PositiveNums(numbers);
+}
+
+// Метод - заполнение массива введенными числами.
+void FillArray(int[] data)
+{
+    for (int i = 0; i < data.Length; i++)
+    {
+        data[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+// Метод - определение количества чисел больше 0.
+void PositiveNums(int[] data)
+{
+    int sum = 0;
+    foreach (int k in data)
+    {
+        sum += k > 0 ? 1 : 0;
+    }
+    Console.Write(sum);
+}
+
+// Метод - печать коэффициентов для вывода результата.
+void PrintArray(int[] data)
+{
+    string line = String.Empty;
+    int l = data.Length - 1;
+    double last = data[l];
+    for (int i = 0; i < l; i++)
+    {
+        line += $"{data[i]}, ";
+    }
+    Console.Write(line + $"{last} -> ");
+}
+
+// Вызов комбинированного метода.
+OutResult();
---
+++ Seminar_6/Task43/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задача 43:
+Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+// Комбинированный метод - вывод результата.
+void OutResult()
+{
+    double[,] coef = new double[2, 2];
+    GetCoefficient(coef);
+    PrintArray(coef);
+    LineCrossing(coef);
+}
+
+// Метод - определение коэффициентов для прямых.
+void GetCoefficient(double[,] data)
+{
+    Console.WriteLine("Задание коэффициентов для прямой y = k1 * x + b1 :");
+    Console.Write("Коэффициент b1 = ");
+    data[0, 0] = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Коэффициент k1 = ");
+    data[0, 1] = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Задание коэффициентов для прямой y = k2 * x + b2");
+    Console.Write("Коэффициент b2 = ");
+    data[1, 0] = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Коэффициент k2 = ");
+    data[1, 1] = Convert.ToDouble(Console.ReadLine());
+}
+
+// Метод - нахождение координат точки пересечения двух линий.
+void LineCrossing(double[,] data)
+{
+    double x = (data[1, 0] - data[0, 0]) / (data[0, 1] - data[1, 1]);
+    double y = data[1, 1] * x + data[1, 0];
+    x = Math.Round(x, 1);
+    y = Math.Round(y, 1);
+    Console.Write($" -> ({x}; {y})");
+}
+
+// Метод - печать коэффициентов для вывода результата.
+void PrintArray(double[,] data)
+{
+    Console.Write($"b1 = {data[0, 0]}, ");
+    Console.Write($"k1 = {data[0, 1]}, ");
+    Console.Write($"b2 = {data[1, 0]}, ");
+    Console.Write($"k2 = {data[1, 1]} ");
+}
+
+// Вызов комбинированного метода.
+OutResult();
