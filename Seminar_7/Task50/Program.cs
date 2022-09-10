f39638d4a48feb8d9f0adb7d544f3b3dccf40ad2
Дмитрий <109813121+YGDragon@users.noreply.github.com>
2022-09-11 01:45:57 +0300

    Update Program.cs
    Code optimization

--- Seminar_7/Task50/Program.cs
+++ Seminar_7/Task50/Program.cs
@@ -1,13 +1,12 @@
 ﻿/*
 Задача 50.
-Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+Напишите программу, которая на вход принимает
+позиции элемента в двумерном массиве,
 и возвращает значение этого элемента
 или же указание, что такого элемента нет.
 Например, задан массив:
 1 4 7 2
-
 5 9 2 3
-
 8 4 2 4
 17 -> такого числа в массиве нет 
 */
@@ -17,40 +16,40 @@ void OutResult()
 {
     int[,] data = new int[4, 4];
     GetArray(data);
-    Console.Write("Введите позицию элемента двумерного массива: ");
+    Console.Write("Ввод позиции элемента двумерного массива: ");
     int position = Convert.ToInt32(Console.ReadLine());
     СheckPosition(data, position);
 }
 
-// Метод - заполнение массива случайными числами и вывод на печать.
+// Метод - заполнение массива и вывод на печать.
 void GetArray(int[,] data)
 {
     for (int i = 0; i < data.GetLength(0); i++)
     {
         for (int j = 0; j < data.GetLength(1); j++)
         {
-            data[i, j] = new Random().Next(40);
+            data[i, j] = new Random().Next(20);
             Console.Write($"{data[i, j]}\t");
         }
         Console.WriteLine('\n');
     }
 }
 
-// Метод - вывод значения элемента в массиве.
+// Метод - проверка позиции элемента в массиве.
 void СheckPosition(int[,] data, int inputPar)
 {
-    int count = -1;
     if (inputPar > data.Length - 1)
     {
         Console.Write($"{inputPar} -> такого числа в массиве нет");
     }
+    int count = 0;
     foreach (int k in data)
     {
-        count++;
         if (count == inputPar)
         {
-            Console.Write(inputPar + "->" + k);
+            Console.Write(inputPar + " -> " + k);
         }
+        count++;
     }
 }
 
