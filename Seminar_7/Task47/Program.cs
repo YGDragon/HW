5ea76c08ee4a5ee30386300d7f869daa89d01412
ygdragon <djsdv@tut.by>
2022-09-10 23:08:31 +0300

    Программы к Seminar_7 для задач №47,50,52

--- Seminar_7/Task47/Program.cs
+++ Seminar_7/Task47/Program.cs
@@ -10,41 +10,21 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 */
 
-// Комбинированный метод - задание размера массива и вывод результата.
-void OutResult()
-{
-    double[,] data = new double[5, 6];
-    FillArray(data);
-    PrintArray(data);
-}
+double[,] data = new double[5, 6];
+GetArray(data);
 
 // Метод - заполнение массива случайными вещественными числами.
-void FillArray(double[,] data)
+void GetArray(double[,] data)
 {
     for (int i = 0; i < data.GetLength(0); i++)
     {
         for (int j = 0; j < data.GetLength(1); j++)
         {
-            double factor = new Random().NextDouble();
-            data[i, j] = factor * new Random().Next(-50, 50);
-            data[i, j] = Math.Round(data[i, j], 1);
-        }
-    }
-}
-
-// Метод - вывод на печать массива.
-void PrintArray(double[,] data)
-{
-    Console.WriteLine();
-    for (int i = 0; i < data.GetLength(0); i++)
-    {
-        for (int j = 0; j < data.GetLength(1); j++)
-        {
-            Console.Write($"{data[i, j]}\t");
+            double coef = 1;
+            coef = coef == 0 ? -1 : new Random().NextDouble();
+            data[i, j] = coef * new Random().Next(-50, 50);
+            Console.Write($"{Math.Round(data[i, j], 1)}\t");
         }
         Console.WriteLine('\n');
     }
 }
-
-// Вызов комбинированного метода.
-OutResult();
