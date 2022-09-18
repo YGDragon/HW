90fc711d2688603dff4b968bcdf3649db2ffc755
ygdragon <djsdv@tut.by>
2022-09-18 04:19:12 +0300

    Code optimization Task62

--- Seminar_8/Task62/Program.cs
+++ Seminar_8/Task62/Program.cs
@@ -10,44 +10,144 @@
 
 //Комбинированный метод - вывод результата.
 void OutResult()
-{
+{   // исходный двумерный массив
     string[,] spiral = new string[4, 4];
-    // Задание зубчатого массива.
+    TwoFill(spiral);
+    Console.WriteLine("Исходный двумерный массив:");
+    PrintTwoArray(spiral);
+    // исходный зубчатый массив
     string[][] numbers = {
-    new string[] { "01", "02", "03", "04" },
-    new string[] { "12", "13", "14", "05" },
-    new string[] { "11", "16", "15", "06" },
-    new string[] { "10", "09", "08", "07" }
-    };
+    new string[3],
+    new string[3],
+    new string[3],
+    new string[3],
+    new string[2],
+    new string[1],
+    new string[1]};
+    // заполнение и вывод зубчатого массива
+    JaggedFill(numbers);
+    Console.WriteLine("Зубчатый массив чисел:");
+    PrintJagArray(numbers);
+    //Console.WriteLine();
+    // заполнение и вывод зубчатого массива
     SpiralFill(spiral, numbers);
-    Console.WriteLine();
-    Console.WriteLine("Двумерный массив 4 на 4:");
-    PrintArray(spiral);
+    Console.WriteLine("Заполненный двумерный массив:");
+    PrintTwoArray(spiral);
 }
 
-// Метод - спиральное заполнение двумерного массива.
-void SpiralFill(string[,] data, string[][] filler)
+// Метод №1 - заполнение двумерного массива.
+void TwoFill(string[,] dataTwo)
 {
-    for (int i = 0; i < data.GetLength(0); i++)
+    for (int i = 0; i < dataTwo.GetLength(0); i++)
     {
-        for (int j = 0; j < data.GetLength(1); j++)
+        for (int j = 0; j < dataTwo.GetLength(1); j++)
         {
-            data[i, j] = filler[i][j];
+            dataTwo[i, j] = "00";
         }
     }
 }
 
-// Метод - вывод на печать двумерного массива.
-void PrintArray(string[,] data)
+// Метод №2 - заполнение зубчатого массива.
+void JaggedFill(string[][] dataJag)
 {
-    for (int i = 0; i < data.GetLength(0); i++)
+    int coef = 1;
+    for (int i = 0; i < dataJag.GetLength(0); i++)
     {
-        for (int j = 0; j < data.GetLength(1); j++)
+        for (int j = 0; j < dataJag[i].Length; j++)
         {
-            Console.Write($"{data[i, j]} ");
+            string line1 = "0" + Convert.ToString(coef);
+            string line2 = Convert.ToString(coef);
+            dataJag[i][j] = coef < 10 ? line1 : line2;
+            coef++;
+        }
+    }
+}
+
+// Метод №3 - вывод на печать двумерного массива.
+void PrintTwoArray(string[,] dataT)
+{
+    for (int i = 0; i < dataT.GetLength(0); i++)
+    {
+        for (int j = 0; j < dataT.GetLength(1); j++)
+        {
+            Console.Write($"{dataT[i, j]} ");
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
+
+// Метод №4 - вывод на печать зубчатого массива.
+void PrintJagArray(string[][] dataJ)
+{
+    for (int i = 0; i < dataJ.GetLength(0); i++)
+    {
+        for (int j = 0; j < dataJ[i].Length; j++)
+        {
+            Console.Write($"{dataJ[i][j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Метод №5 - спиральное заполнение двумерного массива.
+void SpiralFill(string[,] arrayTwo, string[][] arrayJag)
+{
+    int g = 0; // индекс зубчатого массива
+    int n = 0; // счетчик заполнения 
+    for (n = 0; n < 2; n++)
+    {
+        RigthFill(arrayTwo, arrayJag, n, g);
+        g = g == 4 ? 1 : g + 4;
+    }
+    for (n = 0; n < 2; n++)
+    {
+        DownFill(arrayTwo, arrayJag, n, g);
+        g = g == 5 ? 2 : g + 4;
+    }
+    for (n = 0; n < 2; n++)
+    {
+        LeftFill(arrayTwo, arrayJag, n, g);
+        g = g == 6 ? 3 : g + 4;
+    }
+    UpFill(arrayTwo, arrayJag, g);
+}
+
+// Метод №6 - заполнение массива вправо.
+void RigthFill(string[,] arrayTwo, string[][] arrayJag, int n, int g)
+{
+    for (int j = 0; j < arrayJag[g].Length; j++)
+    {
+        arrayTwo[n, j + n] = arrayJag[g][j];
+    }
+}
+
+// Метод №7 - заполнение массива вниз.
+void DownFill(string[,] arrayTwo, string[][] arrayJag, int n, int g)
+{
+    for (int i = 0; i < arrayJag[g].Length; i++)
+    {
+        arrayTwo[i + 2 * n, 3 - n] = arrayJag[g][i];
+    }
+}
+
+// Метод №8 - заполнение массива влево.
+void LeftFill(string[,] arrayTwo, string[][] arrayJag, int n, int g)
+{
+    for (int j = 0; j < arrayJag[g].Length; j++)
+    {
+        arrayTwo[3 - n, arrayJag[g].Length - j] = arrayJag[g][j];
+    }
+}
+
+// Метод №9 - заполнение массива вверх.
+void UpFill(string[,] arrayTwo, string[][] arrayJag, int g)
+{
+    for (int i = 0; i < arrayJag[g].Length; i++)
+    {
+        arrayTwo[arrayJag[g].Length - i, 0] = arrayJag[g][i];
+    }
+}
+
 OutResult();
